19b7f84c8558a39eed0d565023a7e695c621f1f1
Hectorsong <shc505@mail.ustc.edu.cn>
2022-06-27 23:02:51 +0800

    commit test

--- blog-api/src/main/java/com/mszlu/blog/utils/JWTUtils.java
+++ blog-api/src/main/java/com/mszlu/blog/utils/JWTUtils.java
@@ -4,12 +4,16 @@ import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JWTUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JWTUtils.class);
 
     private static final String jwtToken = "123456Mszlu!@#$$";
 
@@ -20,7 +24,7 @@ public class JWTUtils {
                 .signWith(SignatureAlgorithm.HS256, jwtToken) // 签发算法，秘钥为jwtToken
                 .setClaims(claims) // body数据，要唯一，自行设置
                 .setIssuedAt(new Date()) // 设置签发时间
-                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 60 * 1000));// 一天的有效时间
+                .setExpiration(new Date(System.currentTimeMillis() + 24  * 60 * 60 * 1000));// 一天的有效时间
         String token = jwtBuilder.compact();
         return token;
     }
